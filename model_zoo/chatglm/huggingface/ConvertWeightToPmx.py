[feature][fix] add chatglm1/2 dynamic batching supportfix rotary2d emb bug, add padded attention mask (#23)
* [fix] fix rotary2d emb bug
1. for inference, first chunk tensor, and then transpose weight
2. for ConvertWeightToPmx, first chunk weight, and then transpose

[feature] add padded attention mask support for chatglm and xformer mha

* [feature][fix] chatglm2 support, split loader attn bias to wqkv bias and wo bias, and fix potential chatglm bug

---------

Co-authored-by: shengyunrui <shengyunrui@sensetime.com>

model_zoo/chatglm/huggingface/ConvertWeightToPmx.py
@@ -79,17 +79,33 @@ def write_pmx_model(model_path, input_base_path):
              
         wqkv = wqkv.reshape(n_heads, 3, hidden_dim // n_heads, hidden_dim).transpose(0, 1).reshape(3 * hidden_dim, hidden_dim)
         wq, wk, wv = wqkv.chunk(3, dim=0)   # [(num_heads,3,head_dim), hidden_dim] --> [hidden_dim, hidden_dim]
-        
-        wq = wq.view(n_heads, 2, hidden_dim // n_heads // 2, hidden_dim).transpose(1, 2).reshape(hidden_dim, hidden_dim)
-        wk = wk.view(num_kv_heads, 2, key_value_dim // num_kv_heads // 2, hidden_dim).transpose(1, 2).reshape(key_value_dim, hidden_dim)
+
+        # 先对半切，再分别转置
+        wq0, wq1 = wq.chunk(2, dim=0)
+        wq0 = wq0.reshape(n_heads, 2, hidden_dim // n_heads // 4, hidden_dim).transpose(1,2).reshape(hidden_dim // 2, hidden_dim)
+        wq1 = wq1.reshape(n_heads, 2, hidden_dim // n_heads // 4, hidden_dim).transpose(1,2).reshape(hidden_dim // 2, hidden_dim)
+        wq = torch.cat((wq0, wq1), dim=0)
+
+        wk0, wk1 = wk.chunk(2, dim=0)
+        wk0 = wk0.reshape(num_kv_heads, 2, key_value_dim // num_kv_heads // 4, hidden_dim).transpose(1,2).reshape(key_value_dim // 2, hidden_dim)
+        wk1 = wk1.reshape(num_kv_heads, 2, key_value_dim // num_kv_heads // 4, hidden_dim).transpose(1,2).reshape(key_value_dim // 2, hidden_dim)
+        wk = torch.cat((wk0, wk1), dim=0)
+
         return wq, wk, wv
 
     def unpermute_bias(wqkv_bias, n_heads, num_kv_heads, hidden_dim, key_value_dim):
         wqkv_bias = wqkv_bias.reshape(n_heads, 3, hidden_dim // n_heads).transpose(0, 1).reshape(3 * hidden_dim)
         wq_bias, wk_bias, wv_bias = wqkv_bias.chunk(3)
-        
-        wq_bias = wq_bias.view(n_heads, 2, hidden_dim // n_heads // 2).transpose(1, 2).reshape(hidden_dim)
-        wk_bias = wk_bias.view(num_kv_heads, 2, key_value_dim // num_kv_heads // 2).transpose(1, 2).reshape(key_value_dim)
+
+        wq_bias_0, wq_bias_1 = wq_bias.chunk(2)
+        wq_bias_0 = wq_bias_0.view(n_heads, 2, hidden_dim // n_heads // 4).transpose(1, 2).reshape(hidden_dim // 2)
+        wq_bias_1 = wq_bias_1.view(n_heads, 2, hidden_dim // n_heads // 4).transpose(1, 2).reshape(hidden_dim // 2)
+        wq_bias = torch.cat((wq_bias_0, wq_bias_1))
+
+        wk_bias_0, wk_bias_1 = wk_bias.chunk(2)
+        wk_bias_0 = wk_bias_0.view(num_kv_heads, 2, key_value_dim // num_kv_heads // 4).transpose(1, 2).reshape(key_value_dim // 2)
+        wk_bias_1 = wk_bias_1.view(num_kv_heads, 2, key_value_dim // num_kv_heads // 4).transpose(1, 2).reshape(key_value_dim // 2)
+        wk_bias = torch.cat((wk_bias_0, wk_bias_1))
         
         return wq_bias, wk_bias, wv_bias
 
@@ -123,13 +139,13 @@ def write_pmx_model(model_path, input_base_path):
             f"layers.{layer_i}.ffn_norm.bias": hf_model_state_dict[f"transformer.layers.{layer_i}.post_attention_layernorm.bias"],
         })
 
-        state_dict.update({
-            "tok_embeddings.weight": hf_model_state_dict["transformer.word_embeddings.weight"],
-            "norm.weight": hf_model_state_dict["transformer.final_layernorm.weight"],
-            "norm.bias": hf_model_state_dict["transformer.final_layernorm.bias"],
-            "output.weight": hf_model_state_dict["lm_head.weight"]
-        })
-        torch.save(state_dict, os.path.join(model_path, "model.pth"))
+    state_dict.update({
+        "tok_embeddings.weight": hf_model_state_dict["transformer.word_embeddings.weight"],
+        "norm.weight": hf_model_state_dict["transformer.final_layernorm.weight"],
+        "norm.bias": hf_model_state_dict["transformer.final_layernorm.bias"],
+        "output.weight": hf_model_state_dict["lm_head.weight"]
+    })
+    torch.save(state_dict, os.path.join(model_path, "model.pth"))
 
 def main():
     parser = argparse.ArgumentParser()

model_zoo/llama/modeling/dynamic_batching/Model.py
@@ -44,7 +44,9 @@ class Attention(nn.Module):
             friendly_gqa: bool,
             fused_qkv: bool,
             fused_kvcache: bool,
-            linear_bias_term: bool,
+            attn_wqkv_bias_term: bool,
+            attn_wo_bias_term: bool,
+            rotary_dim: int,
             proc_group: dist.ProcessGroup):
         super().__init__()
 
@@ -55,6 +57,7 @@ class Attention(nn.Module):
         self.num_local_kv_heads = self.num_kv_heads // world_size
         self.num_local_kv_repeats = self.num_local_heads // self.num_local_kv_heads
         self.head_dim = args.hidden_dim // args.num_heads
+        self.rotary_dim = rotary_dim
         self.num_layers = args.num_layers
         self.layer_id = layer_id
         self.cache_quant_bit = args.cache_quant_bit
@@ -70,20 +73,20 @@ class Attention(nn.Module):
         if self.fused_qkv:
             self.wqkv = ColumnParallelLinear(
                 proc_group, args.hidden_dim, args.hidden_dim + 2 * self.num_kv_heads * self.head_dim,
-                bias_term=linear_bias_term, gather_output=False)
+                bias_term=attn_wqkv_bias_term, gather_output=False)
         else:
             self.wq = ColumnParallelLinear(
                 proc_group, args.hidden_dim, args.hidden_dim,
-                bias_term=linear_bias_term, gather_output=False)
+                bias_term=attn_wqkv_bias_term, gather_output=False)
             self.wk = ColumnParallelLinear(
                 proc_group, args.hidden_dim, self.num_kv_heads * self.head_dim,
-                bias_term=linear_bias_term, gather_output=False)
+                bias_term=attn_wqkv_bias_term, gather_output=False)
             self.wv = ColumnParallelLinear(
                 proc_group, args.hidden_dim, self.num_kv_heads * self.head_dim,
-                bias_term=linear_bias_term, gather_output=False)
+                bias_term=attn_wqkv_bias_term, gather_output=False)
         self.wo = RowParallelLinear(
             proc_group, args.hidden_dim, args.hidden_dim,
-            bias_term=linear_bias_term, input_is_parallel=True)
+            bias_term=attn_wo_bias_term, input_is_parallel=True)
 
 
     def forward(self, x: torch.Tensor, attn_mask: Optional[torch.Tensor],
@@ -107,9 +110,11 @@ class Attention(nn.Module):
         # TensorDumper.dump(xk, "layer{}_reshaped_xk".format(self.layer_id))
         # TensorDumper.dump(xv, "layer{}_reshaped_xv".format(self.layer_id))
 
+
         xq, xk = PMX.dynamic_batching.rotary_position_embedding(
                                         xq, xk, seqstarts,
-                                        start_pos, max_seqlen)
+                                        start_pos, max_seqlen, 
+                                        self.rotary_dim)
         # TensorDumper.dump(xq, "layer{}_rotary_position_embedding_out_xq".format(self.layer_id))
         # TensorDumper.dump(xk, "layer{}_rotary_position_embedding_out_xk".format(self.layer_id))
 
@@ -158,9 +163,10 @@ class Attention(nn.Module):
                                             head_dim=self.head_dim,
                                             is_causal=self.auto_causal,
                                             num_kv_heads=0 if self.friendly_gqa else self.num_local_kv_heads)
+        attn = PMX.reshape(attn, (0, -1))
         # TensorDumper.dump(attn, "layer{}_multi_head_attention_out".format(self.layer_id))
 
-        output = self.wo(PMX.reshape(attn, (0, -1)))
+        output = self.wo(attn)
         # TensorDumper.dump(output, "layer{}_reshaped_wo_out".format(self.layer_id))
 
         return output
@@ -206,8 +212,10 @@ class TransformerBlock(nn.Module):
                  friendly_gqa: bool,
                  fused_qkv: bool,
                  fused_kvcache: bool,
-                 attn_linear_bias_term: bool,
+                 attn_wqkv_bias_term: bool,
+                 attn_wo_bias_term: bool,
                  ffn_linear_bias_term: bool,
+                 rotary_dim: int, 
                  proc_group: dist.ProcessGroup):
         super().__init__()
         self.attention = Attention(args,
@@ -215,7 +223,9 @@ class TransformerBlock(nn.Module):
                                    friendly_gqa,
                                    fused_qkv,
                                    fused_kvcache,
-                                   attn_linear_bias_term,
+                                   attn_wqkv_bias_term,
+                                   attn_wo_bias_term,
+                                   rotary_dim=rotary_dim,
                                    proc_group=proc_group)
         self.feed_forward = FeedForward(args, 
                                         layer_id,
@@ -251,8 +261,10 @@ class Transformer(nn.Module):
                  friendly_gqa: bool,
                  fused_qkv: bool,
                  fused_kvcache: bool,
-                 attn_linear_bias_term: bool,
+                 attn_wqkv_bias_term: bool,
+                 attn_wo_bias_term: bool,
                  ffn_linear_bias_term: bool,
+                 rotary_dim: int,
                  proc_group: dist.ProcessGroup):
         super().__init__()
         self.params = params
@@ -279,8 +291,10 @@ class Transformer(nn.Module):
                 friendly_gqa,
                 fused_qkv,
                 fused_kvcache,
-                attn_linear_bias_term,
+                attn_wqkv_bias_term,
+                attn_wo_bias_term,
                 ffn_linear_bias_term,
+                rotary_dim,
                 proc_group=proc_group))
 
         self.norm = SkipRMSNorm(params.hidden_dim, eps=params.norm_eps)
@@ -297,22 +311,22 @@ class Transformer(nn.Module):
         # TensorDumper.dump(h, "emb_out")
 
         _kv_scale = kv_scale
-        TensorDumper.dump(tokens, "token_ids")
-        if attn_mask is not None:
-            TensorDumper.dump(attn_mask, "attn_mask")
+        # TensorDumper.dump(tokens, "token_ids")
+        # if attn_mask is not None:
+        #     TensorDumper.dump(attn_mask, "attn_mask")
         if self.fused_kvcache and attn_mask is not None:
             if kv_scale is None: # mount an empty scale for friendly exporting
                 _kv_scale = torch.empty(0, dtype=h.dtype)
-        TensorDumper.dump(seqstarts, "seqstarts")
-        TensorDumper.dump(kvstarts, "kvstarts")
-        TensorDumper.dump(cachestarts, "cachestarts")
-        TensorDumper.dump(decoding_batches, "decoding_batches")
-        TensorDumper.dump(start_pos, "start_pos")
-        TensorDumper.dump(max_seqlen, "max_seqlen")
-        TensorDumper.dump(max_kvlen, "max_kvlen")
-        TensorDumper.dump(kv_cache, "kv_cache")
-        if kv_scale is not None:
-            TensorDumper.dump(kv_scale, "kv_scale")
+        # TensorDumper.dump(seqstarts, "seqstarts")
+        # TensorDumper.dump(kvstarts, "kvstarts")
+        # TensorDumper.dump(cachestarts, "cachestarts")
+        # TensorDumper.dump(decoding_batches, "decoding_batches")
+        # TensorDumper.dump(start_pos, "start_pos")
+        # TensorDumper.dump(max_seqlen, "max_seqlen")
+        # TensorDumper.dump(max_kvlen, "max_kvlen")
+        # TensorDumper.dump(kv_cache, "kv_cache")
+        # if kv_scale is not None:
+        #     TensorDumper.dump(kv_scale, "kv_scale")
 
         norm = None
         for layer in self.layers:
@@ -327,7 +341,7 @@ class Transformer(nn.Module):
         output = self.output(gathered_h)  # only compute last logits
         # TensorDumper.dump(output, "logits_before_cast")
         output = output.float()
-        TensorDumper.dump(output, "logits")
+        # TensorDumper.dump(output, "logits")
         return output
     
 