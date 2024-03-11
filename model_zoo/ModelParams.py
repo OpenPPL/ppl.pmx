7df3e1e2f782202652f354ca2568669185aa40cc
alcanderian <alcanderian@gmail.com>
2024-03-11 17:46:43 +0800

    [refactor] refactor big code

--- model_zoo/ModelParams.py
+++ model_zoo/ModelParams.py
@@ -30,28 +30,3 @@ class ModelParams:
     num_experts: int = 1
     num_experts_per_token: int = 1
     sliding_window: int = 0
-
-
-@dataclass
-class VisionModelParams(ModelParams):
-    hidden_dim: int = 512
-    intermediate_dim: int = 2048
-
-    num_layers: int = 8
-    num_heads: int = 8
-    num_kv_heads: Optional[int] = None
-
-    norm_eps: float = 1e-5
-
-    cache_quant_bit: int = 8
-    cache_quant_group: int = 8
-
-    cache_layout: int = 0
-    cache_mode: int = 0 # only affected when dynamic_batching == True
-
-    # dynamic_batching: bool = True
-    auto_causal: bool = True
-
-    image_size: int = 224
-    patch_size: int = 32
-    projection_dim: int = 512
--- torch_function/VisionEmbedding.py
+++ torch_function/VisionEmbedding.py
@@ -5,45 +5,45 @@ import torch.nn.functional as F
 
 class VisionEmbedding(torch.autograd.Function):
     @staticmethod
-    def symbolic(g, pixel_values: torch.Value, cls_emb_weight: torch.Value,
-                 patch_emb_weight: torch.Value, pos_emb_weight: torch.Value,
-                 hidden_dim: int, image_size: int, patch_size: int):
+    def symbolic(g, pixel_values: torch.Value, class_weight: torch.Value,
+                 patch_weight: torch.Value, position_weight: torch.Value,
+                 hidden_dim: int, patch_size: int):
         output = g.op('pmx::VisionEmbedding',
-                      pixel_values, cls_emb_weight,
-                      patch_emb_weight, pos_emb_weight,
+                      pixel_values, class_weight,
+                      patch_weight, position_weight,
                       hidden_dim_i=hidden_dim,
-                      image_size_i=image_size,
                       patch_size_i=patch_size)
         return output
 
 
     @staticmethod
-    def forward(self, pixel_values: torch.Value, cls_emb_weight: torch.Value,
-                patch_emb_weight: torch.Value, pos_emb_weight: torch.Value,
-                hidden_dim: int, image_size: int, patch_size: int):
+    def forward(self, pixel_values: torch.Value, class_weight: torch.Value,
+                patch_weight: torch.Value, position_weight: torch.Value,
+                hidden_dim: int, patch_size: int):
+        num_patches = (pixel_values.shape[-1] // patch_size) * (pixel_values.shape[-2] // patch_size)
+
         if torch.onnx.is_in_onnx_export():
-            output = torch.zeros([pixel_values.shape[0], (image_size//patch_size)**2+1, hidden_dim]).to(pixel_values.device)
+            output = torch.zeros([pixel_values.shape[0], num_patches + 1, hidden_dim]).to(pixel_values.device)
             return output
         else:
-            num_patches = (image_size // patch_size) ** 2
             num_positions = num_patches + 1
             position_ids = torch.arange(num_positions).expand((1, -1))
             batch_size = pixel_values.shape[0]
 
-            patch_embeds = F.conv2d(pixel_values, patch_emb_weight, stride=patch_size) # shape -> [batch_size, hidden_dim, grid, grid]
+            patch_embeds = F.conv2d(pixel_values, patch_weight, stride=patch_size) # shape -> [batch_size, hidden_dim, grid, grid]
             patch_embeds = patch_embeds.flatten(2).transpose(1, 2) # shape -> [batch_size, grid*grid, hidden_dim]
-            cls_embeds  = cls_emb_weight.expand(batch_size, 1, -1)
-            pos_embeds = F.embedding(position_ids, pos_emb_weight)
+            cls_embeds  = class_weight.expand(batch_size, 1, -1)
+            pos_embeds = F.embedding(position_ids, position_weight)
 
             embeddings = torch.cat([cls_embeds, patch_embeds], dim=1) + pos_embeds # shape -> [batch_size, grid*grid + 1, hidden_dim]
             return embeddings
 
 
-def vision_embedding(pixel_values: torch.Value, cls_emb_weight: torch.Value,
-                     patch_emb_weight: torch.Value, pos_emb_weight: torch.Value,
-                     hidden_dim: int, image_size: int, patch_size: int) -> torch.Tensor:
-    return VisionEmbedding.apply(pixel_values, cls_emb_weight, patch_emb_weight, pos_emb_weight,
-                                 hidden_dim, image_size, patch_size)
+def vision_embedding(pixel_values: torch.Value, class_weight: torch.Value,
+                     patch_weight: torch.Value, position_weight: torch.Value,
+                     hidden_dim: int, patch_size: int) -> torch.Tensor:
+    return VisionEmbedding.apply(pixel_values, class_weight, patch_weight, position_weight,
+                                 hidden_dim, patch_size)
 
 
 if __name__ == "__main__":
@@ -60,12 +60,12 @@ if __name__ == "__main__":
             self.patch_size = patch_size
             self.num_positions = (self.image_size // self.patch_size) ** 2 + 1
 
-            self.cls_emb_weight = nn.Parameter(torch.randn(self.hidden_dim))
-            self.patch_emb_weight  = nn.Parameter(torch.randn([self.hidden_dim, 3, patch_size, patch_size]))
-            self.pos_emb_weight = nn.Parameter(torch.randn(self.num_positions, self.hidden_dim))
+            self.class_weight = nn.Parameter(torch.randn(self.hidden_dim))
+            self.patch_weight  = nn.Parameter(torch.randn([self.hidden_dim, 3, patch_size, patch_size]))
+            self.position_weight = nn.Parameter(torch.randn(self.num_positions, self.hidden_dim))
 
         def forward(self, pixel_values: torch.Tensor):
-            return vision_embedding(pixel_values, self.cls_emb_weight, self.patch_emb_weight, self.pos_emb_weight, self.hidden_dim, self.image_size, self.patch_size)
+            return vision_embedding(pixel_values, self.class_weight, self.patch_weight, self.position_weight, self.hidden_dim, self.patch_size)
 
     hidden_dim, image_size, patch_size = 512, 224, 32
     test_op1 = TestModule1(hidden_dim, image_size, patch_size)
@@ -76,11 +76,11 @@ if __name__ == "__main__":
     print (model_str1)
     #out = test_op1.forward(pixel_values)
 
-    torch.onnx.export(
-        test_op1,
-        (pixel_values),
-        "vision_embedding.onnx",
-        input_names=['pixel_values'],
-        output_names=['vision_embeddings'],
-        opset_version=11,
-    )
+    # torch.onnx.export(
+    #     test_op1,
+    #     (pixel_values),
+    #     "vision_embedding.onnx",
+    #     input_names=['pixel_values'],
+    #     output_names=['vision_embeddings'],
+    #     opset_version=11,
+    # )
