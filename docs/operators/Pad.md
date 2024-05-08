e101e81407ae8323201a06892f0327941cb213e0
jizhe <jizhe1@sensetime.com>
2024-05-08 08:54:00 +0000

    modify pad input interface

--- docs/operators/Pad.md
+++ docs/operators/Pad.md
@@ -2,16 +2,13 @@
 
 Pad the input tensor similar to torch.nn.functional.pad. 
 
-The padding size by which to pad some dimensions of input are described starting from the last dimension and moving forward.
-
-See torch.nn.CircularPad2d, torch.nn.ConstantPad2d, torch.nn.ReflectionPad2d, and torch.nn.ReplicationPad2d for concrete examples on how each of the padding modes works. 
-
-
 ## Attributes/Parameters
 
-### `mode`: str
+### `mode`: str (default: 'constant')
 
-'constant', 'reflect', 'replicate' or 'circular'. Default: 'constant'
+'constant', 'reflect', 'replicate' or 'circular'.
+
+See torch.nn.CircularPad2d, torch.nn.ConstantPad2d, torch.nn.ReflectionPad2d, and torch.nn.ReplicationPad2d for concrete examples on how each of the padding modes works. 
 
 ## Inputs
 
@@ -21,12 +18,24 @@ N-dimensional tensor
 
 ### `padding`: tensor(int64)
 
-m-elements tensor
+m-elements tensor, where $\frac{m}{2} \leq$ input dimensions and $m$ is even.
+
+The padding size by which to pad some dimensions of input are described starting from the last dimension and moving forward.
+$\left\lfloor\frac{\operatorname{len}(\mathrm{padding})}{2}\right\rfloor$ dimensions of input will be padded.
+
+For example, 
+
+to pad only the last dimension of the input tensor, padding use 
+(padding_left, padding_right); 
+
+to pad the last 2 dimensions of the input tensor, padding use (padding_left, padding_right, padding_top, padding_bottom)
+
+to pad the last 3 dimensions of the input tensor, padding use (padding_left, padding_right, padding_top, padding_bottom, padding_front, padding_back)
 
 
-### `value`:  float
+### `value`:  (optional):tensor(float)
 
-fill value for 'constant' padding. Default: 0
+fill value for 'constant' padding. 
 
 
 ## Outputs
--- torch_function/Pad.py
+++ torch_function/Pad.py
@@ -6,45 +6,44 @@ from typing import Sequence, Union
 class Pad(torch.autograd.Function):
     @staticmethod
     def symbolic(g, input: torch.Value, padding: torch.Value,
-                 mode: str='constant', value: float=0.):
-        output = g.op("opmx::Pad", input, padding,
-                      mode_s = mode,
-                      value_f = value)
+                 value: torch.Value, mode: str='constant'):
+        output = g.op("opmx::Pad", input, padding, value,
+                      mode_s = mode)
         return output
 
 
     @staticmethod
     def forward(self, input: torch.Tensor, padding: torch.Tensor,
-                mode: str='constant', value: float=0.):
-        output = torch.nn.functional.pad(input, tuple(padding), mode, value)
+                value: torch.Tensor, mode: str='constant'):
+        output = torch.nn.functional.pad(input, tuple(padding), mode, value.item())
         return output
 
 
-def pad(input: torch.Tensor, padding: Sequence[Union[int, torch.SymInt]],
-            mode: str='constant', value: float=0.) -> torch.Tensor:
-    return Pad.apply(input, padding, mode, value)
+def pad(input: torch.Tensor, padding: torch.Tensor,
+        value: torch.Tensor, mode: str='constant') -> torch.Tensor:
+    return Pad.apply(input, padding, value, mode)
 
 
 if __name__ == "__main__":
     class TestModule1(torch.nn.Module):
-        def __init__(self, mode='constant', value: float=0.) -> None:
+        def __init__(self, mode='constant') -> None:
             super().__init__()
             self.mode = mode
-            self.value = value
 
 
-        def forward(self, input: torch.Tensor, padding: Sequence[Union[int, torch.SymInt]]):
-            return pad(input, padding, self.mode, self.value)
+        def forward(self, input: torch.Tensor, padding: torch.Tensor, value: torch.Tensor):
+            return pad(input, padding, value, self.mode)
 
 
     test_op1 = TestModule1()
 
     input = torch.ones([1, 32, 512, 512])
     padding = torch.tensor([0,1,0,1], dtype=torch.int32)
+    value = torch.tensor(0)
     #pad = [0,1,0,1]
 
-    output = test_op1.forward(input, padding)
+    output = test_op1.forward(input, padding, value)
     model_str1 = torch.onnx.export_to_pretty_string(
-        test_op1, (input, padding), "pad.onnx", opset_version=11)
+        test_op1, (input, padding, value), "pad.onnx", opset_version=11)
 
     print(model_str1)
