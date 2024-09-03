a28435e8633603bd6982bce15e900b0ad09ceb56
jizhe <jizhe1@sensetime.com>
2024-09-03 07:46:24 +0000

    modify convert file

--- model_zoo/qwen2/huggingface/ConvertWeightToOpmx.py
+++ model_zoo/qwen2/huggingface/ConvertWeightToOpmx.py
@@ -12,22 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import argparse
-import gc
 import json
 import os
-import shutil
-import warnings
 
 import torch
 
 from pathlib import Path
+from safetensors.torch import load_file
+
 
 """
 Sample usage:
 
 ```
 python convert_hf_weights_to_pmx.py \
-    --input_dir /path/to/downloaded/hf/weights/7B --output_dir /output/path
+    --input_dir /input/path --output_dir /output/path
 ```
 
 Thereafter, models can be loaded via:
@@ -51,7 +50,7 @@ def write_json(text, path):
         json.dump(text, f)
 
 
-def write_pmx_model(model_path, input_base_path, model_type):
+def write_pmx_model(model_path, input_base_path):
     os.makedirs(model_path, exist_ok=True)
     print ("Loading the checkpoint in a HF model")
 
@@ -64,6 +63,9 @@ def write_pmx_model(model_path, input_base_path, model_type):
     pmx_params_dict['norm_eps'] = params['rms_norm_eps']
     pmx_params_dict['vocab_size'] = params['vocab_size']
     pmx_params_dict['num_kv_heads'] = params.get('num_key_value_heads', params['num_attention_heads'])
+    pmx_params_dict['rope_theta'] = params['rope_theta']
+    pmx_params_dict["max_position_embeddings"] = params["max_position_embeddings"]
+
 
     # compute intermediate_size
     hidden_dim = pmx_params_dict['hidden_dim']
@@ -73,55 +75,50 @@ def write_pmx_model(model_path, input_base_path, model_type):
         pmx_params_dict['intermediate_dim'] = params.get("intermediate_size")
     else:
         pmx_params_dict['intermediate_dim'] = compute_intermediate_size(hidden_dim, ffn_dim_multiplier, multiple_of)
-
-    for key in ('max_position_embeddings', 'rope_theta'):
-        if key in params:
-            pmx_params_dict[key] = params[key]
-
-    print(pmx_params_dict)
     write_json(pmx_params_dict, os.path.join(model_path, "opmx_params.json"))
 
     # TO DO: GQA / MQA, only test on llama
     num_heads = pmx_params_dict['num_heads']
     num_kv_heads = pmx_params_dict['num_kv_heads']
     dims_per_head = hidden_dim // num_heads
+    # key_value_dim = pmx_params_dict['hidden_dim']
     key_value_dim = dims_per_head * num_kv_heads
 
     # load weights
-    def unpermute(w, n_heads=num_heads, dim1=hidden_dim, dim2=hidden_dim):
-        return w.view(n_heads, 2, dim1 // n_heads // 2, dim2).transpose(1, 2).reshape(dim1, dim2)
+    def unpermute(w, n_heads=num_heads, dim_in=hidden_dim, dim_out=hidden_dim):
+        return w.view(n_heads, 2, dim_out // n_heads // 2, dim_in).transpose(1, 2).reshape(dim_out, dim_in)
+
+
+    def unpermute_bias(bias, n_heads=num_heads, dim=hidden_dim):
+        return bias.view(n_heads, 2, dim // n_heads // 2).transpose(1, 2).reshape(dim)
 
     hf_model_state_dict, state_dict = {}, {}
-
-    if model_type is None:
-        if any(Path(input_base_path).glob("*.safetensors")):
-            model_type = "safetensors"
-        else:
-            model_type = "bin"
-
-    if model_type == "bin":
-        for ckpt_path in sorted(Path(input_base_path).glob("*.bin")):
-            hf_model_state_dict.update(torch.load(ckpt_path, map_location="cpu"))
-    elif model_type == "safetensors":
-        from safetensors import safe_open
-        for ckpt_path in sorted(Path(input_base_path).glob("*.safetensors")):
-            weights = safe_open(ckpt_path, 'pt', 'cpu')
-            weights = {k: weights.get_tensor(k) for k in weights.keys()}
-            hf_model_state_dict.update(weights)
-    else:
-        raise ValueError(f"Not support the model_type: {model_type}.")
+    for ckpt_path in sorted(Path(input_base_path).glob("*.safetensors")):
+        hf_model_state_dict.update(load_file(ckpt_path, device="cpu"))
 
     for layer_i in range(pmx_params_dict['num_layers']):
+        wq = unpermute(hf_model_state_dict[f"model.layers.{layer_i}.self_attn.q_proj.weight"], num_heads, hidden_dim, hidden_dim)
+        wk = unpermute(hf_model_state_dict[f"model.layers.{layer_i}.self_attn.k_proj.weight"], num_kv_heads, hidden_dim, key_value_dim)
 
-        wq = unpermute(hf_model_state_dict[f"model.layers.{layer_i}.self_attn.q_proj.weight"])
-        wk = unpermute(hf_model_state_dict[f"model.layers.{layer_i}.self_attn.k_proj.weight"], num_kv_heads, key_value_dim, hidden_dim)
+        wq_bias = hf_model_state_dict[f"model.layers.{layer_i}.self_attn.q_proj.bias"]
+        wk_bias = hf_model_state_dict[f"model.layers.{layer_i}.self_attn.k_proj.bias"]
+
+        wq_bias = unpermute_bias(wq_bias, num_heads, hidden_dim)
+        wk_bias = unpermute_bias(wk_bias, num_kv_heads, key_value_dim)
+
         wv = hf_model_state_dict[f"model.layers.{layer_i}.self_attn.v_proj.weight"]
 
         state_dict.update({
             f"layers.{layer_i}.attention.wq.weight": wq,
             f"layers.{layer_i}.attention.wk.weight": wk,
             f"layers.{layer_i}.attention.wv.weight": wv,
+
+            f"layers.{layer_i}.attention.wq.bias": wq_bias,
+            f"layers.{layer_i}.attention.wk.bias": wk_bias,
+            f"layers.{layer_i}.attention.wv.bias": hf_model_state_dict[f"model.layers.{layer_i}.self_attn.v_proj.bias"],
+
             f"layers.{layer_i}.attention.wo.weight": hf_model_state_dict[f"model.layers.{layer_i}.self_attn.o_proj.weight"],
+            # f"layers.{layer_i}.attention.wo.bias": hf_model_state_dict[f"model.layers.{layer_i}.self_attn.o_proj.bias"],
             f"layers.{layer_i}.feed_forward.w1.weight": hf_model_state_dict[f"model.layers.{layer_i}.mlp.gate_proj.weight"],
             f"layers.{layer_i}.feed_forward.w2.weight": hf_model_state_dict[f"model.layers.{layer_i}.mlp.down_proj.weight"],
             f"layers.{layer_i}.feed_forward.w3.weight": hf_model_state_dict[f"model.layers.{layer_i}.mlp.up_proj.weight"],
@@ -132,32 +129,25 @@ def write_pmx_model(model_path, input_base_path, model_type):
     state_dict.update({
         "tok_embeddings.weight": hf_model_state_dict["model.embed_tokens.weight"],
         "norm.weight": hf_model_state_dict["model.norm.weight"],
-        "output.weight": hf_model_state_dict["lm_head.weight"]
+        "output.weight": hf_model_state_dict["model.embed_tokens.weight"],
     })
+
+
     torch.save(state_dict, os.path.join(model_path, "model.pth"))
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--input_dir",
-        help="Location of HF weights, which contains tokenizer.model and model folders",
-    )
+         "--input_dir",
+         help="Location of HF weights, which contains tokenizer.model and model folders",)
     parser.add_argument(
-        "--output_dir",
-        help="Location to write OPMX model",
-    )
-    parser.add_argument(
-        "--model_type",
-        choices=["bin", "safetensors"],
-        default=None,
-        help="Input model type",
-    )
+         "--output_dir",
+         help="Location to write OPMX model",)
     args = parser.parse_args()
     write_pmx_model(
         model_path=args.output_dir,
         input_base_path=args.input_dir,
-        model_type=args.model_type
     )
 
 if __name__ == "__main__":
